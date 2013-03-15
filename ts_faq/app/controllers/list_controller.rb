ed79242b097dfede08a6136b217d4b3f2776bfb5
MikuMurakami <Murakami_Miku@ogis-ri.co.jp>
2013-03-15 13:09:50 +0900

    一覧表示＋検索機能完成

--- ts_faq/app/controllers/list_controller.rb
+++ ts_faq/app/controllers/list_controller.rb
@@ -1,5 +1,35 @@
 class ListController < ApplicationController
   def index
-  	@list = Item.all
+    @list = Item.all
   end
+  
+  def search
+    # キーワード
+    @keyWord = params[:keyWord]
+  
+    # 項目の検索
+    @list = Item.find(:all, :conditions => ["name LIKE ?", "%" + @keyWord + "%"])
+    
+    # ケースの検索
+    @cases = Case.find(:all, :conditions => ["name LIKE ?", "%" + @keyWord + "%"])
+    @cases.each do |a_case|
+      if !(@list.include?(a_case.item)) then
+        @list.push a_case.item
+      end
+    end
+    
+    # 手順の検索
+    @procedures = Procedure.find(:all, :conditions => ["name LIKE ?", "%" + @keyWord + "%"])
+    @procedures.each do |procedure|
+      if !(@list.include?(procedure.case.item)) then
+        @list.push procedure.case.item
+      end
+    end
+    
+    # ソート
+    @list.sort!{|a, b| a.id <=> b.id}
+    
+    render :action => 'index'
+  end
+  
 end
